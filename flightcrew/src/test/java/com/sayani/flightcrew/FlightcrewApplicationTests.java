cffac185e3fef9c9ebc140a4f8a7018047af2cb0
Sayani Dey <121103461+sayanidey95@users.noreply.github.com>
2026-05-08 21:41:04 +0000

    Initial Spring Boot SetUp

---
+++ flightcrew/src/test/java/com/sayani/flightcrew/FlightcrewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sayani.flightcrew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightcrewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
